9abe6ad094f02404710549df97226aad405c8f58
Sean Enck <enckse@gmail.com>
2017-07-05 16:35:43 -0400

    updated hook...

--- hooks/epiphyte-motd.sh
+++ hooks/epiphyte-motd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MOTD=/etc/motd
 echo > $MOTD
+echo "system status" >> $MOTD
 echo "===" >> $MOTD
 cat /var/log/pacman.log | grep "starting full system upgrade" | tail -n 1 | cut -d " " -f 1 | sed "s/\[//g;s/$/ -> full system upgrade/g" >> $MOTD
 echo "===" >> $MOTD
