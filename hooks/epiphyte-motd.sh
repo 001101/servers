8ee744d8a91db25822bd14a606466d45bc1517cf
Sean Enck <enckse@gmail.com>
2017-07-05 16:52:39 -0400

    cleaner version

--- hooks/epiphyte-motd.sh
+++ hooks/epiphyte-motd.sh
@@ -4,6 +4,8 @@ echo > $MOTD
 echo "system status" >> $MOTD
 echo "===" >> $MOTD
 cat /var/log/pacman.log | grep "starting full system upgrade" | tail -n 1 | cut -d " " -f 1 | sed "s/\[//g;s/$/ -> full system upgrade/g" >> $MOTD
-pacman -Sl epiphyte | grep installed | sed "s/^epiphyte //g;s/^/  epiphyte.version -> /g" >> $MOTD
+echo >> $MOTD
+echo "epiphyte:" >> $MOTD
+pacman -Sl epiphyte | grep installed | sed "s/^epiphyte //g;s/^/  version -> /g" >> $MOTD
 echo "===" >> $MOTD
 echo >> $MOTD
