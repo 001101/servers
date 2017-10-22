2900823e4080818e93d86601ac8ecdfe134b1344
Sean Enck <enckse@gmail.com>
2017-10-22 15:28:36 -0400

    directive for bash

--- hooks/bootctl-update.sh
+++ hooks/bootctl-update.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 version=$(bootctl status 2>/dev/null | grep "systemd\-boot "| sed -n -e 's/^.*systemd-boot//p' | sed 's/[^0-9]*//g' | uniq)
 current=$(bootctl --version | head -n 1 | cut -d " " -f 2)
 if [ ! -z "$version" ]; then
