3780cdc28136badd07cba637c500adbd06d8c99c
Sean Enck <enckse@gmail.com>
2017-10-22 15:29:39 -0400

    just do a ==> WARNING like mkinit

--- hooks/bootctl-update.sh
+++ hooks/bootctl-update.sh
@@ -3,11 +3,6 @@ version=$(bootctl status 2>/dev/null | grep "systemd\-boot "| sed -n -e 's/^.*sy
 current=$(bootctl --version | head -n 1 | cut -d " " -f 2)
 if [ ! -z "$version" ]; then
     if [ $version != $current ]; then
-        echo "===NOTICE==="
-        read -p "bootctl update ($version != $current) (y/n)? " yn
-        if [[ $yn == "y" ]]; then
-            bootctl update
-        fi
-        echo "===NOTICE==="
+        echo "==> WARNING: bootctl update required ($version != $current)"
     fi
 fi
