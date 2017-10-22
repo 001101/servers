2109d0429cf87a4b0a283181d162b5ed990ac700
Sean Enck <enckse@gmail.com>
2017-10-22 15:14:39 -0400

    moving here

---
+++ hooks/bootctl-update.sh
@@ -0,0 +1,10 @@
+version=$(bootctl status 2>/dev/null | grep "systemd\-boot "| sed -n -e 's/^.*systemd-boot//p' | sed 's/[^0-9]*//g' | uniq)
+current=$(bootctl --version | head -n 1 | cut -d " " -f 2)
+if [ $version != $current ]; then
+    echo "===NOTICE==="
+    read -p "bootctl update ($version != $current) (y/n)? " yn
+    if [[ $yn == "y" ]]; then
+        bootctl update
+    fi
+    echo "===NOTICE==="
+fi
