f76666a6d20f21c856e7a7541038d165b1f74b4d
Sean Enck <enckse@gmail.com>
2017-10-26 05:28:21 -0400

    moved to pkgbuilds

--- hooks/bootctl-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-version=$(bootctl status 2>/dev/null | grep "systemd\-boot "| sed -n -e 's/^.*systemd-boot//p' | sed 's/[^0-9]*//g' | uniq)
-current=$(bootctl --version | head -n 1 | cut -d " " -f 2)
-if [ ! -z "$version" ]; then
-    if [ $version != $current ]; then
-        echo "==> WARNING: bootctl update required ($version != $current)"
-    fi
-fi
